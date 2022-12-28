b6cc87f97f862ea5bc0ccfba1b1c211fbbadae95
AgungStudent <672021077@student.uksw.edu>
2022-12-28 21:18:43 +0700

    crud product

--- routes/api.php
+++ routes/api.php
@@ -15,12 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('products', \App\Http\Controllers\ProductController::class);
 });
 
-Route::resource('products', \App\Http\Controllers\ProductController::class);
 
-//Route::middleware('guest')->group(function () {
-Route::post('/login', [LoginController::class, 'login'])->name('login');
-//});
+Route::middleware('guest')->group(function () {
+    Route::post('/login', [LoginController::class, 'login'])->name('login');
+});
